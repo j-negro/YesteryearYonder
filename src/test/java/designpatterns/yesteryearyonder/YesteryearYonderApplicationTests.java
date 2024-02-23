6fb071a15c5513dacddeb67ea9161faef60ae0e7
Juan Negro <jnegro@itba.edu.ar>
2024-02-23 13:23:29 +0100

    Init SpringBoot project

---
+++ src/test/java/designpatterns/yesteryearyonder/YesteryearYonderApplicationTests.java
@@ -0,0 +1,13 @@
+package designpatterns.yesteryearyonder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesteryearYonderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
