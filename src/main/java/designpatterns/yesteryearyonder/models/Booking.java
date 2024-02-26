33ae19456954f6fb044a18cf60a53dd1435dcc0b
Princess Rosette Dela Paz <princessrosettedelapaz@gmail.com>
2024-02-26 07:28:27 +0100

    Updated Booking model by introducing a 'state' attribute.
    to represent the current state of bookings

--- src/main/java/designpatterns/yesteryearyonder/models/Booking.java
+++ src/main/java/designpatterns/yesteryearyonder/models/Booking.java
@@ -2,15 +2,9 @@ package designpatterns.yesteryearyonder.models;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import designpatterns.yesteryearyonder.interfaces.services.BookingState;
+import designpatterns.yesteryearyonder.models.states.ConfirmedBookingState;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "bookings")
@@ -36,6 +30,14 @@ public class Booking {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "timemachine_id")
     private TimeMachine timeMachine;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state", nullable = false)
+    private BookingState state;
+    public BookingState getState() {
+        return state;
+    }
 
     public Booking() {
     }
@@ -70,6 +72,10 @@ public class Booking {
 
     public LocalDate getEndDate() {
         return endDate;
+    }
+
+    public void setState(BookingState state) {
+        this.state = state;
     }
 
 }
